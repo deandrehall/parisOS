8082bb4994e79c8b3faf517b9746647f859c700b
Deandre Hall <deandrehall00@gmail.com>
2017-01-26 20:03:57 -0600

    instruction scheduling almost done

--- ass1/assignment1.cpp
+++ ass1/assignment1.cpp
@@ -38,6 +38,7 @@ class CPU{
 
         CPU(int numCores){
             this->numCores = numCores;
+
             for(int i=0; i<numCores; i++){
                 core newCore;
                 newCore.coreID = i;
@@ -50,7 +51,7 @@ class CPU{
         ~CPU(){
         }
 
-        void freeCore();
+        void freeCore(int);
         void setBusyUntil(int, int);
         core requestCore();
         int retFreeCoreID();
@@ -61,6 +62,8 @@ struct instruction{
     //int arrival;
     int duration;
     int remaining;
+    int currentLocation;
+    int processID;
     //std::string status;
 };
 
@@ -101,14 +104,17 @@ int main(){
     inputFile.open("input.txt");
      
     instruction newInstruction;
+    int processIDdummy = -1;
     Process newProcess;
     CPU cpu;
 
     string command = "";
     int val = 0;
 
-    while(inputFile>>command>>val){
+    while(cin >> command >> val){
+
         cout<<command<<"\t"<<val<<endl;
+
         if(command == "NCORES"){
            cpu = CPU(val); 
         }
@@ -122,34 +128,48 @@ int main(){
         else{
             newInstruction.name = command; 
             newInstruction.duration = val;
-            newInstruction.remaining = val;
-            
+            newInstruction.processID = ++processIDdummy;
             processList.back().instrList.push_back(newInstruction);
         }
     }
 
-    while(processList.size() >= 0){
+    while(processList.size() > 0){
         instruction processFront = processList.front().instrList.front(); 
         if(processFront.name == "CORE"){
            //if process requests core time and a core is free mark that
            //core as busy 
             if(cpu.requestCore().coreID >= 0){
+                //if duration<=SLICE
                 if(processFront.duration <= SLICE){
                     cpu.setBusyUntil(CLOCK+processFront.duration,
                             processList.front().retPID());
                     processList.front().instrList.front().duration=0;
+                    cout<<processFront.name<<" has been completed"<<endl;
+                    cpu.freeCore(processList.front().instrList.front().currentLocation); 
                 }
+                //if duration>SLICE
                 else{
                     cpu.setBusyUntil(CLOCK+SLICE,
                             processList.front().retPID());
                     processList.front().instrList.front().duration -= SLICE;
+
+                    cout<<"sending instruction "<<processList.front().instrList.front().name<<" to readyQueue with "<< processList.front().instrList.front().duration<<" remaining"<<endl;
+
+                    cpu.freeCore(processList.front().instrList.front().currentLocation); 
                 }
             }
             //if no core is free, add the process to the readyQueue and
             //remove that process from the top of the processList
             else{
+                cout<<"sending "<<processFront.name<<" to ready queue with "<<processFront.duration<<" remaining"<<endl;
                 readyQueue.push_back(processList.front());
                 processList.erase(processList.begin());
+            }
+            if(processList.front().instrList.front().duration == 0){
+                //print report
+                cout<<"instruction "<<processList.front().instrList.front().name<<" has been completed. removing it from processList"<<endl; 
+                //pop instruction from processList
+                processList.front().instrList.erase(processList.front().instrList.begin()); 
             }
 
 
@@ -175,12 +195,12 @@ int CPU::retFreeCoreID(){
     return this->requestCore().coreID;
 }
 
-void CPU::freeCore(){
+void CPU::freeCore(int coreID){
     this->numCores--;
-    int coreID = this->requestCore().coreID;
     this->cores[coreID].currentProcessID = -1;
     this->cores[coreID].busyUntil = 0;
     this->cores[coreID].active = false;
+    
 }
 
 void CPU::setBusyUntil(int reqTime, int pID){
@@ -189,6 +209,7 @@ void CPU::setBusyUntil(int reqTime, int pID){
     this->cores[coreID].currentProcessID = pID; 
     this->cores[coreID].busyUntil = reqTime;
     this->cores[coreID].active = true;
+    
 }
 
 core CPU::requestCore(){
