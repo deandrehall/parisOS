8e5601bb355df6aff45e82f3d0494310c19bb7ec
Deandre Hall <deandrehall00@gmail.com>
2017-03-16 13:08:58 -0500

    basic setup for both files

--- ass2/client.cpp
+++ ass2/client.cpp
@@ -1,1 +1,18 @@
-ok nice
+//Deandre Hall
+//PSID: 1241692
+//COSC 3360
+//Assignment 2: A public key server
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
---
+++ ass2/server.cpp
@@ -0,0 +1,18 @@
+//Deandre Hall
+//PSID: 1241692
+//COSC 3360
+//Assignment 2: A public key server
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
