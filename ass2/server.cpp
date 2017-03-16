ffb50701346de8a37585de21cfed310750a45467
Deandre Hall <deandrehall00@gmail.com>
2017-03-16 13:35:22 -0500

    added input file parsing

--- ass2/server.cpp
+++ ass2/server.cpp
@@ -9,10 +9,30 @@
 #include <vector>
 #include <fstream>
 #include <cstdlib>
+#include <sys/socket.h>
+#include <netdb.h>
 
 using namespace std;
 
+struct student{
+    string username = "";
+    string key = "";
+};
+
 int main(){
+
+    string filename = "";
+    string username = "";
+    string key = "";
+
+    cout<<"Enter a file name: ";
+    cin>>filename;
+    fstream inputFile;
+    inputFile.open(filename);
+
+    while(inputFile >> username >> key){
+        cout<<username<<"\t"<<key<<endl;
+    }
 
     return 0;
 }
